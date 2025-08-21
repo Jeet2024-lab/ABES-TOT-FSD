226abf12e98ed424c2655622f3226ebac887d28f
Jitendra Kumar Chauhan <jitendra.chauhan@abes.ac.in>
2025-08-21 11:12:50 +0530

    JS Array object

---
+++ Day5 JS/script.js
@@ -0,0 +1,23 @@
+const employee = {
+    name: "John Doe",   
+    age: 30,
+    position: "Software Engineer",
+    department: "Development",
+    planguage:{
+        name: "JavaScript",
+        version: "ES6"
+    },
+    skills: ["JavaScript", "React", "Node.js"],
+    isActive: true,
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+        zip: "12345"
+    },
+}
+console.log(employee);
+employee.name = "Jitendra Kumar Chauhan";
+console.log(employee.name);
+employee.skills[1] = "Angular";
+console.log(employee.skills);
