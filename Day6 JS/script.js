10bd7dd91e6db4dbac45d7ce6d3af2da71a2753d
Jitendra Kumar Chauhan <jitendra.chauhan@abes.ac.in>
2025-08-22 11:01:05 +0530

    Callback example

--- Day6 JS/script.js
+++ Day6 JS/script.js
@@ -4,31 +4,38 @@
 // 1)Callback
 
 
-function register(){
-    setTimeout(()=>{console.log("register end")});
+function register(cb){
     console.log("register user");
+    setTimeout(()=>{console.log("register end")},1000);
+    cb(login);
+    
 }
-function sendEmail(){
+function sendEmail(cb){
     console.log("send user Email");
-    setTimeout(()=>{console.log("Send user Email End")});
+    setTimeout(()=>{console.log("Send user Email End")},2000);
+    cb(getData);
 }
-function login(){
+function login(cb){
     console.log("login here");
-    setTimeout(()=>{console.log("Login End")});
+    setTimeout(()=>{console.log("Login End")},3000);
+    cb(displayData)
 }
-function getData(){
+function getData(cb){
     console.log("getData");
-    setTimeout(()=>{console.log("getData end ")});
+    setTimeout(()=>{console.log("getData end ")},4000);
+    cb();
 }
 function displayData(){
     console.log("display user Data");
-    setTimeout(()=>{console.log("display user Data end ")});
+    setTimeout(()=>{console.log("display user Data end ")},1000);
 }
 
-register();
+register(sendEmail);
+// function cb(){
 
-sendEmail();
-login();
-getData();
-displayData();
+// sendEmail();
+// login();
+// getData();
+// displayData();
+// }
 console.log("call other application");
