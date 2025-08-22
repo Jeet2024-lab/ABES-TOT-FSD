8c6601a109d9f5d76bc0e816bb64a8edff8ebb12
Jitendra Kumar Chauhan <jitendra.chauhan@abes.ac.in>
2025-08-22 11:27:10 +0530

    CallBack Hell completed

--- Day6 JS/script.js
+++ Day6 JS/script.js
@@ -6,31 +6,34 @@
 
 function register(cb){
     console.log("register user");
-    setTimeout(()=>{console.log("register end")},1000);
-    cb(login);
+    setTimeout(()=>{console.log("register end");cb();},1000);
+    // cb(login);
+    
     
 }
 function sendEmail(cb){
     console.log("send user Email");
-    setTimeout(()=>{console.log("Send user Email End")},2000);
-    cb(getData);
+    setTimeout(()=>{console.log("Send user Email End");cb();},2000);
+    // cb(getData);
+    
 }
 function login(cb){
     console.log("login here");
-    setTimeout(()=>{console.log("Login End")},3000);
-    cb(displayData)
+    setTimeout(()=>{console.log("Login End");cb();},3000);
+    // cb(displayData);
+    
 }
 function getData(cb){
     console.log("getData");
-    setTimeout(()=>{console.log("getData end ")},4000);
-    cb();
+    setTimeout(()=>{console.log("getData end ");cb();},4000);
+    
 }
 function displayData(){
     console.log("display user Data");
     setTimeout(()=>{console.log("display user Data end ")},1000);
 }
 
-register(sendEmail);
+// register(sendEmail);
 // function cb(){
 
 // sendEmail();
@@ -38,4 +41,16 @@ register(sendEmail);
 // getData();
 // displayData();
 // }
+
+
+register(()=>{
+    sendEmail(()=>{
+        login(()=>{
+            getData(()=>{
+                displayData();
+            });
+        });
+    });
+});
+
 console.log("call other application");
